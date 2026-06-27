46792c66c7500e709d64eed5aafbc047b7585c65
Vilca-12 <juancarlosvilca.hijo@gmail.com>
2026-06-27 18:22:29 +0000

    fix(CKV2_AWS_76): agregar regla AMR AnonymousIpList al WAF para proteccion Log4j

--- infraestructure/terraform/waf.tf
+++ infraestructure/terraform/waf.tf
@@ -85,8 +85,27 @@ resource "aws_wafv2_web_acl" "main" {
 
   tags = { Name = "${var.project_name}-waf" }
   rule {
-    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    name     = "AWSManagedRulesAnonymousIpList"
     priority = 4
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAnonymousIpList"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AnonymousIpListMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 5
     override_action {
       none {}
     }
