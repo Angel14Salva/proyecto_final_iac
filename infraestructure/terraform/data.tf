9e50762a619a6b2f0ac24b5529e8c36fac2fef22
Angel14Salva <salvadorluis290@gmail.com>
2026-06-27 17:56:34 +0000

    feat(data): agregar VPC Endpoint Gateway para DynamoDB

--- infraestructure/terraform/data.tf
+++ infraestructure/terraform/data.tf
@@ -228,6 +228,14 @@ data "aws_iam_policy_document" "dynamodb_kms_policy" {
   }
 }
 
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.dynamodb"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+  tags = { Name = "${var.project_name}-vpc-endpoint-dynamodb" }
+}
+
 resource "aws_kms_alias" "dynamodb" {
   name          = "alias/${var.project_name}/dynamodb"
   target_key_id = aws_kms_key.dynamodb.key_id
