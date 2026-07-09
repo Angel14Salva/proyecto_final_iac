875d79cb06c85aff00c4920c6e1c2baabdf28028
Angel14Salva <salvadorluis290@gmail.com>
2026-07-09 05:36:45 +0000

    fix(data): corregir KMS policy de DynamoDB con acciones y resources validos

--- infraestructure/terraform/data.tf
+++ infraestructure/terraform/data.tf
@@ -237,8 +237,8 @@ data "aws_iam_policy_document" "dynamodb_kms_policy" {
       type        = "AWS"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
-    actions   = ["kms:"]
-    resources = [""]
+    actions   = ["kms:*"]
+    resources = ["*"]
   }
 }
 
